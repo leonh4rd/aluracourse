57bf792f240dce080a200388047eb4391c9fe7f3
Leonardo <leonardo@quetzal.com.br>
2019-10-08 11:49:41 -0300

    Consultado XML com LINQ

--- linqtoxml/Program.cs
+++ linqtoxml/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace linqtoxml
 {
@@ -10,7 +12,39 @@ namespace linqtoxml
 	{
 		static void Main(string[] args)
 		{
+			string xml =
+				"<Movies>" +
+					"<Movie>" +
+						"<Director>Quentin Tarantino</Director>" +
+						"<Title>Pulp Fiction</Title>" +
+						"<Minutes>154</Minutes>" +
+					"</Movie>" +
+					"<Movie>" +
+						"<Director>James Cameron</Director>" +
+						"<Title>Avatar</Title>" +
+						"<Minutes>162</Minutes>" +
+					"</Movie>" +
+				"</Movies>";
+
+			//XmlDocument document = new XmlDocument();
+			//document.LoadXml(xml);
+
+			XDocument document = XDocument.Parse(xml);
+
+			IEnumerable<XElement> query =
+				from f in document.Descendants("Movie")
+				select f;
+
 			Console.WriteLine("LINQ to XML");
+			Console.WriteLine();
+
+			foreach (var item in query)
+			{
+				Console.WriteLine(item.Element("Director").FirstNode);
+				Console.WriteLine(item.Element("Title").FirstNode);
+			}
+
+			Console.ReadKey();
 		}
 	}
 }
