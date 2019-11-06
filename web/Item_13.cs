d3ce2c2c4ba94f25831bb51d83cee220ecc3152a
Leonardo <leonardo@quetzal.com.br>
2019-11-06 13:45:21 -0300

    Parte8_Aula5-1

---
+++ web/Item_13.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace web
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Tarefas de WebRequest
+			// Conectar ao site Caelum (http://www.caelum.com.br)
+			// Obter código html
+			// Exibir código html
+
+			WebRequest request = WebRequest.Create("http://www.caelum.com.br");
+
+			WebResponse response = request.GetResponse();
+
+			using (var stream = new StreamReader(response.GetResponseStream()))
+			{
+				string code = stream.ReadToEnd();
+				Console.WriteLine(code);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
